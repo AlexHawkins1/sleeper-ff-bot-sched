129287f8336ff05cca80973b0cc4768574feff67
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-09-10 13:05:00 -0400

    Initial Commit of app

---
+++ README.md
@@ -0,0 +1,113 @@
+![Version](https://img.shields.io/badge/Version-v0.0.1-blue)
+[![Build Status](https://travis-ci.org/SwapnikKatkoori/sleeper-ff-bot.svg?branch=master)](https://travis-ci.org/SwapnikKatkoori/sleeper-ff-bot)
+![Version](https://img.shields.io/badge/license-MIT-pink)
+![GitHub issues](https://img.shields.io/github/issues/SwapnikKatkoori/sleeper-ff-bot)
+<h1 align="center">Welcome to sleeper-ff-bot 👋</h1>
+<p>
+</p>
+
+All credit to u/SwapniKatkoori for creating the bot
+
+A GroupMe/Slack/Discord Bot for Sleeper fantasy leagues. It sends messages on the schedule below.
+
+## Current Schedule
+- Thursday:
+     - 7pm ET Matchups for the week.
+- Friday:
+     - 9am ET Scores
+- Sunday:
+     - 7pm ET Close games.
+- Monday:
+     - 9am ET Scores
+     - 8pm ET Close games
+- Tuesday:
+     - 11am ET league standing.
+     - 11:01am ET Highest scoring team, lowest scoring team, most points left on the bench, and teams that started players that scored negative.
+
+
+## Setup
+1. [ GroupMe ](#groupme)
+2. [ Slack ](#slack)
+3. [ Discord ](#discord)
+
+<a name="groupme"></a>
+### GroupMe
+- Step 1: Go to https://dev.groupme.com/ and log in using your groupme account.
+- Step 2: Click the bots tab.
+- Step 3: Click the "Create Bot" button.
+- Step 4: Choose the groupchat that the bot will be in and the Name of the bot and click "Submit". Everything else can be left empty.
+- Step 5: Remember the bot_id as you will need it in the next step.
+<img src="/Media/groupme/groupme_botid.jpeg" width="400"/>
+
+- Step 6: Follow directions to launch the bot on a Heroku server [here](#heroku)
+
+<a name="slack"></a>
+### Slack
+- Step 1: Go to https://api.slack.com/apps and click "Create New App".
+- Step 2: Choose the name you want to call the FF bot, choose your workspace, and click "Create App".
+- Step 3: Click the "Incoming Webhooks" button.
+<img src="/Media/Slack/incoming_webhooks.jpeg" width="400"/>
+
+- Step 4: Click the "Off" toggle button so it says "On".
+<img src="/Media/Slack/toggle.jpeg" width="400"/>
+
+- Step 5: Click the "Add New Webhook to Workspace" button.
+<img src="/Media/Slack/add_new_webhook.jpeg" width="400"/>
+
+- Step 6: Select the channel to post to and click "Install".
+- Step 7: Remember the webhook that appears under the "Webhook URL" section as you will need it for the next step.
+<img src="/Media/Slack/copy_webhook.jpeg" width="400"/>
+
+- Step 8: Follow directions to launch the bot on a Heroku server [here](#heroku)
+
+<a name="discord"></a>
+### Discord
+- Step 1: Go to the Discord server that you want to add the bot to.
+- Step 2: Go to "Server Settings".
+<img src="/Media/discord/server_settings.jpeg" width="400"/>
+
+- Step 3: Click "Webhooks" in the side menu.
+- Step 4: Click "Create Webhook" and fill out the information.
+- Step 5: Copy the "Webhook Url" as you will need it in step 7.
+<img src="/Media/discord/webhook.jpeg" width="400"/>
+
+- Step 6: Click "Save".
+
+- Step 7: Follow directions to launch the bot on a Heroku server [here](#heroku)
+
+<a name="heroku"></a>
+## Deploy the bot
+- Step 1: Go to https://signup.heroku.com/login and create a Heroku account.
+- Step 2: Click this button to deploy the Bot:
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/SwapnikKatkoori/sleeper-ff-bot)
+- Step 3: Choose an app name
+
+- Step 4:
+
+For Groupme fill out the following (The BOT_TYPE needs to be groupme):
+
+<img src="/Media/groupme/enviornment_setup.jpeg" width="400"/>
+
+For Slack fill out the following (The BOT_TYPE needs to be slack):
+
+<img src="/Media/Slack/enviornment_setup.jpeg" width="400"/>
+
+For Discord fill out the following (The BOT_TYPE needs to be discord):
+
+<img src="/Media/discord/enviornment_setup.jpeg" width="400"/>
+
+You can leave everything else as their default values.
+
+- Step 5: Click "Deploy app".
+- Step 6: After the deployment process is done, click "Manage app" and go to the "Resources" tab.
+- Step 7: Click the pencil icon next to worker and toggle it to the on position, and click "confirm".
+<img src="/Media/deployment/toggle.jpeg" width="400"/>
+
+And you are all done! The bot should now be deployed an you should get a welcome message.
+
+## Author
+
+👤 **Swapnik Katkoori**
+
+## License
+This project is licensed under the terms of the MIT license.
---
+++ app.py
@@ -0,0 +1,134 @@
+from sleeper_ff_bot.bot import get_draft_order, send_any_string, get_player_name, get_fun_fact, get_rule_changes, get_standings_string, get_scores_string, get_matchups_string, get_player_key,  get_player_stats, get_random_player, get_team_abbr, get_depth_chart, find_position
+from sleeper_ff_bot.group_me import GroupMe
+from sleeper_ff_bot.slack import Slack
+from sleeper_ff_bot.discord import Discord
+import os
+import json
+import time
+import logging
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+bot = None
+
+bot_type = os.environ["BOT_TYPE"]
+league_id = os.environ["LEAGUE_ID"]
+
+
+if bot_type == "groupme":
+    bot_id = os.environ["BOT_ID"]
+    bot = GroupMe(bot_id)
+elif bot_type == "slack":
+    webhook = os.environ["SLACK_WEBHOOK"]
+    bot = Slack(webhook)
+elif bot_type == "discord":
+    webhook = os.environ["DISCORD_WEBHOOK"]
+    bot = Discord(webhook)
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    # 'message' is an object that represents a single GroupMe message.
+    message = request.get_json()
+    if not sender_is_bot(message):
+        time.sleep(3)
+        if os.environ["WAITING_FOR_RESPONSE"] == "True":
+            try:
+                int(message['text'].lower())
+                stat_response = True
+            except:
+                stat_response = False
+            if stat_response == True:
+                get_player_key(message['text'],message['name'].lower(),1)
+                os.environ["WAITING_FOR_RESPONSE"] = "False"
+            elif len(message['text']) == 3:
+                get_player_key(message['text'],message['name'].lower(),1)
+            if 'none' in message['text'].lower():
+                bot.send(send_any_string,"Sorry I failed you.")
+                os.environ["WAITING_FOR_RESPONSE"] = "False"
+        if '@bot' in message['text'].lower():
+            if 'draft order' in message['text'].lower():
+                bot.send(get_draft_order)
+            elif 'who' in message['text'].lower():
+                if 'quarterback' in message['text'].lower() or ' qb ' in message['text'].lower() or 'passing' in message['text'].lower():
+                    if 'best' in message['text'].lower() or 'most' in message['text'].lower():
+                        best_any_flag = 'best'
+                    else:
+                        best_any_flag = 'any'
+                    text = get_random_player('qb',best_any_flag)
+                if 'running back' in message['text'].lower() or ' rb ' in message['text'].lower() or 'rushing' in message['text'].lower():
+                    if 'best' in message['text'].lower() or 'most' in message['text'].lower():
+                        best_any_flag = 'best'
+                    else:
+                        best_any_flag = 'any'
+                    text = get_random_player('rb',best_any_flag)
+                if 'wide receiver' in message['text'].lower() or ' wr ' in message['text'].lower() or 'receiving' in message['text'].lower():
+                    if 'best' in message['text'].lower() or 'most' in message['text'].lower():
+                        best_any_flag = 'best'
+                    else:
+                        best_any_flag = 'any'
+                    text = get_random_player('wr',best_any_flag)
+                if 'tight end' in message['text'].lower() or ' te ' in message['text'].lower():
+                    if 'best' in message['text'].lower() or 'most' in message['text'].lower():
+                        best_any_flag = 'best'
+                    else:
+                        best_any_flag = 'any'
+                    text = get_random_player('te',best_any_flag)
+                if 'defense' in message['text'].lower() or ' def ' in message['text'].lower():
+                    text = get_random_player('def', 'any')
+                if 'kicker' in message['text'].lower() or ' k ' in message['text'].lower() or 'field goal' in message['text'].lower()  or 'extra point' in message['text'].lower()  or 'fg' in message['text'].lower()  or 'xp' in message['text'].lower():
+                    if 'best' in message['text'].lower() or 'most' in message['text'].lower():
+                        best_any_flag = 'best'
+                    else:
+                        best_any_flag = 'any'
+                    text = get_random_player('k',best_any_flag)
+
+                bot.send(send_any_string, text)
+            elif 'fun fact' in message['text'].lower():
+                bot.send(get_fun_fact)
+            elif 'rule changes' in message['text'].lower():
+                bot.send(get_rule_changes)
+            elif 'standings' in message['text'].lower():
+                bot.send(get_standings_string, league_id)
+            elif 'score update' in message['text'].lower():
+                bot.send(get_scores_string, league_id)
+            elif 'matchups' in message['text'].lower():
+                bot.send(get_matchups_string, league_id)
+            elif 'stats special' in message['text'].lower():
+                text = message['text']
+                text = text.replace("@bot","")
+                text = text.replace("stats special", "")
+                text = text.replace(" ","")
+                text = text.lower()
+                get_player_key(text,message['name'].lower(),1)
+            elif 'stats' in message['text'].lower():
+                text = message['text']
+                text = text.replace("@bot","")
+                text = text.replace("stats", "")
+                text = text.replace(" ","")
+                text = text.lower()
+                waiting = get_player_key(text, message['name'].lower(),0)
+                os.environ["WAITING_FOR_RESPONSE"] = waiting
+            elif 'depth chart' in message['text'].lower():
+                team_abbr = get_team_abbr(message['text'].lower())
+                position = find_position(message['text'].lower())
+                if position == "error" and team_abbr == "error":
+                    bot.send(send_any_string, 'I was unable to determine what team or position you are looking for.')
+                elif position == "error":
+                    bot.send(send_any_string, 'I was unable to determine what position you are looking for.')
+                elif team_abbr == "error":
+                    bot.send(send_any_string, 'I was unable to determine what team you are looking for.')
+                else:
+                    get_depth_chart(team_abbr.strip(), position.strip())
+            else:
+                bot.send(send_any_string, 'I am unsure.')
+
+    return "ok",200
+
+# Checks whether the message sender is a bot
+def sender_is_bot(message):
+	return message['sender_type'] == "bot"
